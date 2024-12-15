18fca0dfd5f777da17399f451d3081c4c8bf699b
Phillip England <phillip.mark.england@gmail.com>
2024-12-15 14:23:14 -0600

    added date support in meta data

--- main.go
+++ main.go
@@ -51,6 +51,7 @@ func main() {
         "PostName": "[phillip england]",
         "SubText": "learning in public",
         "ImagePath": "/static/img/me2.jpg",
+        "DateWritten": "🎂 born on 12/9/2024",
 			})
 		} else {
 			vbf.WriteString(w, "404 not found")
@@ -72,6 +73,7 @@ func main() {
       "PostName": "[posts]",
       "SubText": "see what i've been learning about",
       "ImagePath": "/static/img/posts.webp",
+      "DateWritten": "🎂 born on 12/9/2024",
 		})
 	}, vbf.MwLogger)
 
@@ -112,11 +114,16 @@ func main() {
       }
       metaDataElm := doc.Find("#meta-data")
       var subText string
+      var dob string
       metaDataElm.Find("*").Each(func(i int, sel *goquery.Selection) {
         key, _ := sel.Attr("key")
         if key == "subtext" {
           val, _ := sel.Attr("value")          
           subText = val
+        }
+        if key == "dob" {
+          val, _ := sel.Attr("value")
+          dob = val
         }
       })
       vbf.ExecuteTemplate(w, templates, "root.html", map[string]interface{}{
@@ -126,71 +133,11 @@ func main() {
         "PostName": "["+ strings.ReplaceAll(title, "-", " ") +"]",
         "SubText": subText,
         "ImagePath": imagePath,
+        "DateWritten": "written " +dob,
       })
     }, vbf.MwLogger)
     return nil
   })
-
-	// vbf.AddRoute("GET /post/trying-neovim", mux, gCtx, func(w http.ResponseWriter, r *http.Request) {
-	// 	templates, _ := vbf.GetContext(KeyTemplates, r).(*template.Template)
-	// 	mdContent, err := vbf.LoadMarkdown("./content/post/0.trying-neovim.md", "dracula")
-	// 	if err != nil {
-	// 		fmt.Println(err.Error())
-	// 		w.WriteHeader(500)
-	// 		return
-	// 	}
-	// 	vbf.ExecuteTemplate(w, templates, "root.html", map[string]interface{}{
-	// 		"Title":   "phillip england",
-	// 		"Content": template.HTML(mdContent),
-	// 		"ReqPath": r.URL.Path,
-	// 	})
-	// }, vbf.MwLogger)
-	//
-	//  vbf.AddRoute("GET /post/async-rust", mux, gCtx, func(w http.ResponseWriter, r *http.Request) {
-	// 	templates, _ := vbf.GetContext(KeyTemplates, r).(*template.Template)
-	// 	mdContent, err := vbf.LoadMarkdown("./content/post/1.async-rust.md", "dracula")
-	// 	if err != nil {
-	// 		fmt.Println(err.Error())
-	// 		w.WriteHeader(500)
-	// 		return
-	// 	}
-	// 	vbf.ExecuteTemplate(w, templates, "root.html", map[string]interface{}{
-	// 		"Title":   "phillip england",
-	// 		"Content": template.HTML(mdContent),
-	// 		"ReqPath": r.URL.Path,
-	// 	})
-	// }, vbf.MwLogger)
-	//
-	//  vbf.AddRoute("GET /post/formatting-strings-in-rust", mux, gCtx, func(w http.ResponseWriter, r *http.Request) {
-	// 	templates, _ := vbf.GetContext(KeyTemplates, r).(*template.Template)
-	// 	mdContent, err := vbf.LoadMarkdown("./content/post/2.formatting-strings-in-rust.md", "dracula")
-	// 	if err != nil {
-	// 		fmt.Println(err.Error())
-	// 		w.WriteHeader(500)
-	// 		return
-	// 	}
-	// 	vbf.ExecuteTemplate(w, templates, "root.html", map[string]interface{}{
-	// 		"Title":   "phillip england",
-	// 		"Content": template.HTML(mdContent),
-	// 		"ReqPath": r.URL.Path,
-	// 	})
-	// }, vbf.MwLogger)
-	//
-	//  vbf.AddRoute("GET /post/random-numbers-in-rust", mux, gCtx, func(w http.ResponseWriter, r *http.Request) {
-	// 	templates, _ := vbf.GetContext(KeyTemplates, r).(*template.Template)
-	// 	mdContent, err := vbf.LoadMarkdown("./content/post/3.random-numbers-in-rust.md", "dracula")
-	// 	if err != nil {
-	// 		fmt.Println(err.Error())
-	// 		w.WriteHeader(500)
-	// 		return
-	// 	}
-	// 	vbf.ExecuteTemplate(w, templates, "root.html", map[string]interface{}{
-	// 		"Title":   "phillip england",
-	// 		"Content": template.HTML(mdContent),
-	// 		"ReqPath": r.URL.Path,
-	// 	})
-	// }, vbf.MwLogger)
-
 
 	err = vbf.Serve(mux, "8080")
 	if err != nil {
